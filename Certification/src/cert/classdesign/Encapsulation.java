ceac494f6bbf120abce74f62a4c44d9a220efa20
dierivera777 <50347146+dierivera777@users.noreply.github.com>
2020-03-02 06:09:30 -0500

    Encapasulation & inheritance

--- Certification/src/cert/classdesign/Encapsulation.java
+++ Certification/src/cert/classdesign/Encapsulation.java
@@ -1,5 +1,20 @@
 package cert.classdesign;
 
+/**One of the four fundamental object oriented programming concepts
+ * inheritance ,polymorphism and abstraction*/
 public class Encapsulation {
+	
+	/**Inmmutability , no setter methods , initialization through constructor*/
+	
+	public Encapsulation(Integer salary) {
+		this.salary = salary;
+	}
+
+	private Integer salary;
+	
+	public void raiseSalary(Integer raiseAmmount) {
+		this.salary=this.salary + raiseAmmount;
+		
+	}
 
 }
--- Certification/src/cert/classdesign/Inheritance.java
+++ Certification/src/cert/classdesign/Inheritance.java
@@ -1,5 +1,34 @@
 package cert.classdesign;
 
-public class Inheritance {
+public class Inheritance extends Encapsulation {
 
+	public Inheritance(Integer salary,String name) {
+		super(salary);
+		this.name=name;
+	}
+	
+	private String name;
+	
+	public void changeName(String newName) {
+		this.name=newName;
+		
+	}
+	
+class Test{
+	
+	public void test() {
+	Encapsulation test1= new Inheritance(2000,"Matt");
+	test1.raiseSalary(2000);
+	//test1.changeName("Albert");
+	
+	Inheritance test2 = new Inheritance(2000,"Peter");
+	test2.raiseSalary(2000);
+	test2.changeName("Albert");
+	
+	Inheritance test3=(Inheritance) test1;
+	test3.changeName("Sussie");
+	
+	
+	}
+}
 }
